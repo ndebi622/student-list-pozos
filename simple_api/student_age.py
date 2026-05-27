4dd2b73f4350a6974ed35026a9aeb873b3d1190a
Christian Gregory Ndebi <ndoumbendebi@gmail.com>
2026-05-27 16:51:38 +0100

    Update student_age.py

--- simple_api/student_age.py
+++ simple_api/student_age.py
@@ -55,4 +55,4 @@ def not_found(error):
     return make_response(jsonify({'error': 'Not found'}), 404)
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    app.run(host='0.0.0.0', port=5000, debug=False)
